472292b3a1c968a713437d075e09ed839fffb899
AllTheCodeWeCannotSee <183735517@qq.com>
2023-09-13 10:55:51 +0800

    Songs fetching and list display

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,10 +1,16 @@
-import Image from 'next/image'
+
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
+import getSongs from "@/actions/getSongs";
+import PageContent from "@/app/(site)/components/PageContent";
 
-export default function Home() {
-  return (
-    <div className="
+export const revalidate = 0;
+
+export default async function Home() {
+    const songs = await getSongs();
+
+    return (
+        <div className="
     bg-neutral-900
     rounded-lg
     h-full
@@ -12,18 +18,18 @@ export default function Home() {
     overflow-hidden
     overflow-y-auto
     ">
-      <Header>
-        <div className="mb-2">
-            <h1
-                className="
+            <Header>
+                <div className="mb-2">
+                    <h1
+                        className="
                     text-white
                     text-3xl
                     font-semibold
                 ">
-                Welcome back
-            </h1>
-            <div
-                className="
+                        Welcome back
+                    </h1>
+                    <div
+                        className="
                     grid
                     grid-cols-1
                     sm:grid-cols-2
@@ -32,24 +38,24 @@ export default function Home() {
                     gap-3
                     mt-4
                 ">
-                <ListItem
-                    image="/images/liked.png"
-                    name="Liked Songs"
-                    href="liked"
-                />
+                        <ListItem
+                            image="/images/liked.png"
+                            name="Liked Songs"
+                            href="liked"
+                        />
+                    </div>
+                </div>
+            </Header>
+            <div className="mt-2 mb-7 px-6">
+                <div className="flex justify-between items-center">
+                    <h1 className="text-white text-2xl font-semibold">
+                        Newest songs
+                    </h1>
+                </div>
+                <div>
+                    <PageContent songs={songs}/>
+                </div>
             </div>
         </div>
-      </Header>
-        <div className="mt-2 mb-7 px-6">
-            <div className="flex justify-between items-center">
-                <h1 className="text-white text-2xl font-semibold">
-                    Newest songs
-                </h1>
-            </div>
-            <div>
-                List of Songs!
-            </div>
-        </div>
-    </div>
-  )
+    )
 }
